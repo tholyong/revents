a87838fd91e52f4bce91db85a98b217f5540515e
Lim Thol Yong <ty.lim_438@hotmail.com>
2018-10-15 23:24:50 +0800

    End of Section 6 React router

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css'
-import './index.css';
-import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 
 let render = () => {
-  ReactDOM.render(<App />, rootEl);
-}
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootEl
+  );
+};
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App', () => {
-    setTimeout(render)
-  })
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 
 render();
