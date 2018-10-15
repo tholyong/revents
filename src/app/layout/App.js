39bd67288aeabcd313cbe5bec439deee430aedd3
Lim Thol Yong <ty.lim_438@hotmail.com>
2018-10-15 12:53:35 +0800

    End of Section 3

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/event/EvenDashboard";
+import NavBar from "../../features/nav/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-vents</h1>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
